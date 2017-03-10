eaa261fab5a7412ec5989b4859b88fd3801d7b95
张泽 <zhangzeStrong@126.com>
2017-03-10 13:53:33 +0800

    swift基本语法
    swift基本语法学习
    1：变量
    2：常量
    3：int类型
    4：float类型
    5：double类型

---
+++ 基本语法/MyPlayground.playground/Contents.swift
@@ -0,0 +1,29 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//基础语法
+
+//变量和常量的声明，以及基本数据类型。
+var b:Int = 23
+
+var a:Int = 2
+
+let c = 23
+
+let d:Float = 3
+
+let e:Double = 3.56
+
+var f = 2, g = 4, h = 45
+print(f, g, h)
+var i = 100_23_34_34
+
+var j = 1_2_3_4_5
+
+let k:Float = Float(d) + Float(e)
+
+//布尔类型
+
+
+
